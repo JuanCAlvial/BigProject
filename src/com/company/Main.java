143003a9a69ed8164edcc1555b59cfdca6ad1060
JuanCAlvial <juancalvial@gmail.com>
2021-01-27 20:48:28 -0500

    SUN feat added with bug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,9 @@ public class Main {
         FactorX.Moon=2;
         FactorX.Planets=2;
 
+        SUN trayo = new SUN();
+        trayo.temp = "123465";
+        trayo.Heat = "40000k";
+        trayo.Radius = "30146969546km";
     }
 }
---
+++ src/com/company/SUN.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class SUN {
+
+    int temp;
+    String Heat;
+    String Radius;
+
+
+}
