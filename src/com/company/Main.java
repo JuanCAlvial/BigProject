026cc59e8888aa33a8debca449dcbc454e7d475b
JuanCAlvial <juancalvial@gmail.com>
2021-01-27 21:50:18 -0500

    Planet2 is ready

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        SolarSystem FactorX = new SolarSystem();
+​
+​
+        SolarSystem FactorX =new SolarSystem();
         FactorX.Sun=1;
         FactorX.Moon=2;
-        FactorX.Planets=2;
-
-        SUN trayo = new SUN();
-        trayo.temp = "123465";
-        trayo.Heat = "40000k";
-        trayo.Radius = "30146969546km";
+        FactorX.planets=2;
+​
+​
+        SUN troyo = new SUN();
+        troyo.Heat="56748kj";
+        troyo.temperature="347878k";
+        troyo.Radius="99899898km";
+​
+         Planet2 Omaro =new Planet2();
+        Fearo.color="blue";
+        Fearo.Radius="878893278937958km";
+​
     }
 }
